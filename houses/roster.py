2739f88545c8d4b0dd4be5c46a0c89386452f68a
Senay Yakut <senaykt@gmail.com>
2020-08-04 16:21:47 -0700

    DataScience Project Houses Solution 1

--- houses/roster.py
+++ houses/roster.py
@@ -1,1 +1,14 @@
-# TODO
+
+import cs50
+from sys import argv, exit
+
+#If the incorrect number of command-line arguments are provided
+if len(argv) != 2:
+    print("missing command-line argument")
+    exit(1)
+# set up a database connection
+db = cs50.SQL("sqlite:///students.db")    
+# query the students table in the students.db database for all of the students in the specified house.    
+rows = db.execute("SELECT * FROM students WHERE house = ?", argv[-1])
+for row in rows:
+    print(row["first"] + " " + row["middle"]+ " " if(row["middle"]) else ""+ " " + row["last"] + ", born" + str(row["birth"]))
